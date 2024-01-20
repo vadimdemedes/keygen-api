96e9a8aed7c194255d804cf4f817caf246e23c28
Vadim Demedes <vadimdemedes@hey.com>
2024-01-20 15:26:06 +0200

    Fix migration

--- db/migrate/20160927003417_rolify_create_roles.rb
+++ db/migrate/20160927003417_rolify_create_roles.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class RolifyCreateRoles < ActiveRecord::Migration
+class RolifyCreateRoles < ActiveRecord::Migration[7.0]
   def change
     create_table(:roles) do |t|
       t.string :name
